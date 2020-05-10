f65fd09f987ccd2e2e67587015403c3d99288eb7
Debbs1 <62051244+Debbs1@users.noreply.github.com>
2020-05-10 17:08:29 +0100

    fixed some errors

--- task4.js
+++ task4.js
@@ -12,26 +12,76 @@ const retryBtn = document.getElementById('retry-btn');
 let shuffleQuestions, index
 
 startBtn.addEventListener('click', startGame);
+nextBtn.addEventListener('click', () => {
+	index++
+	setNextQuestion()
+})
 function startGame(){
 	startBtn.classList.add('hide')
 	score.classList.remove('hide')
 	questionContainer.classList.remove('hide')
 	nextBtn.classList.remove('hide')
-	shuffleQuestions = questions.sort(() => Math.random () -.5)
+	shuffleQuestions = question.sort(() => Math.random () -.5)
 	index= 0
 	nextQuestion();
-};
+}
 
 function nextQuestion(){
-	showQuestion(shuffleQuestions[index])
+	resetState();
+	currentQuestion = shuffleQuestions[index]
+	showQuestion(currentQuestion);
+}
 
 function showQuestion(question){
-	questionElement.innertext = question.question
+	questionElement.innerText = question.question
+	question.answers.forEach(answer =>{
+		const button = document.createElement('button')
+		button.innerTextm = answer.text
+		button.classList.add('btn')
+		if (answer.correct){
+			button.dataset.correct = answer.correct	
+		}
+		button.addEventListener('click', selectAns)
+		answers.appendChild(button)
+	})
 }
-
+function resetState(){
+	clearStatusClass(document.body)
+	nextBtn.classList.add('hide')
+	while (answerBtn.firstChild){
+		answerBtn.removeChild (answerBtn.firstChild)
+	}
 }
-function selectAns (){}
+function selectAns (e){
+	const selectedBtn= e.target
+	const correct = selectedBtn.dataset.correct
+	setStatusClass(document.body, correct)
+	Array.from(answerButtonsElement.children).forEach(button =>{
+		setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffleQuestions.length > index + 1){
+    	nextBtn.classList.remove('hide')
+    	}else {
+    		startBtn.innerText = 'retry'
+    		retry-btn.classList.remove ('hide')
+    	}
+	
+    }
+    
+ function setStatusClass(element, correct){
+ 	clearStatusClass(element)
+ 	if(correct){
+ 		element.classList.add('correct')
+ 		}else{
+ 			element.classList.add('wrong')}
+ 	}
 
+ 	function clearStatusClass(element){
+ 		element.classList.remove('correct')
+ 		element.classList.remove('wrong')
+ 	
+ 	}
+ 
 
 const question = [
     {
@@ -41,10 +91,9 @@ const question = [
         {text:'Peggy', correct: true},
         {text:'Jane', correct: false},
         {text:'Tony', correct: false}]
-        
     },
 
-    {
+ {
         question: "Why did Thanos want to destroy half of the world?",
         answers: [
         {text:'fun', correct: false},
@@ -81,6 +130,8 @@ const question = [
         {text:'Mjolnir', correct: true},
         {text:'Odin', correct: false},
         {text:'Nameless', correct: false}]
-      
+        
     }
-    ];
+    ]; 
+
+   
